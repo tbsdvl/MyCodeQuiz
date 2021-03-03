755e9e14beb4d8454e07dd7938798c0c27a71154
tbsdvl <tristonburns97@gmail.com>
2021-03-02 22:22:19 -0700

    Updating question span text to insert question text

--- logic.js
+++ logic.js
@@ -5,15 +5,27 @@ var time = 10;
 var startButton = document.querySelector("#start");
 var secondsLeft = document.querySelector("#seconds");
 var answersBox = document.querySelector("#answers");
+var questionBox = document.querySelector("#quizquestion")
 var quizQuestions = {
     q1: "How would one begin and end a multiline comment inside a JavaScript file?",
     q2: "What is the syntax for a conditional ternary operator?",
     q3: "Which of the following is an example of a curry function?",
     q4: "What are the three main components of Behavior Driven Development?"
 };
+
+var randomQuestion = Math.floor(Math.random() * Object.entries(quizQuestions).length);
 var quizChoices = {
     choices1: ["//, \\", "/*, */", "*|, |*", "#/, #/"]
 };
+
+var buttons = {
+    button1: document.querySelector("#answer1"),
+    button2: document.querySelector("#answer2"),
+    button3: document.querySelector("#answer3"),
+    buttons4: document.querySelector("#answer4")
+};
+
+var quizAnswers = [quizChoices.choices1[1]];
 
 
 // LOGIC
@@ -22,6 +34,7 @@ var quizChoices = {
 //     I am presented with a question
 
 startButton.addEventListener("click", function() {
+    questionBox.textContent = (JSON.stringify(quizQuestions.q1));
     var quizTimer = setInterval(function() {
         secondsLeft.textContent = (--time + " Seconds left.");
         console.log(time);
@@ -37,16 +50,29 @@ startButton.addEventListener("click", function() {
 });
 
 // Populate answer buttons with choices
-function answerPopulate(){
-for(i in answersBox.children){
-    answersBox.children[i].textContent = JSON.stringify(quizChoices.choices1[i]);
-    };
-};
-
+// function answerPopulate() {
+// for(i in answersBox.children){
+//     answersBox.children[i].textContent = JSON.stringify(quizChoices.choices1[i]);
+//     };
+//     questionsPopulate();
+// };
 
 // answer a question
+// function clickAnswer() {
+//     buttons.addEventListener("click", function() {
+//     var buttonClick = buttonClick.textContent;
+//     });
+//     questionsPopulate();
+// };
 
 //     presented with another question
+// function questionsPopulate() {
+//     for(i in quizQuestions){
+//         if(clickAnswer() === quizAnswers[i]){
+//             questionBox.textContent = quizQuestions[i + 1];
+//         };
+//     };
+// };
 
 // if i answer a question incorrectly
 //     time is subtracted from the clock
