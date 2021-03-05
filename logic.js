823692d9430f758d71a5e8fc06f6a4fc5a743e6a
tbsdvl <tristonburns97@gmail.com>
2021-03-04 18:13:24 -0700

    Adding conditions if user chooses correct answer one point is added to their win record.

--- logic.js
+++ logic.js
@@ -3,6 +3,7 @@
 // Variables and Objects
 var time = 10;
 var clickCount = 0;
+var winsCount = 0;
 var startButton = document.querySelector("#start");
 var secondsLeft = document.querySelector("#seconds");
 var answersBox = document.querySelector("#answers");
@@ -31,13 +32,8 @@ var buttons = {
     button1: document.querySelector("#answer1"),
     button2: document.querySelector("#answer2"),
     button3: document.querySelector("#answer3"),
-    buttons4: document.querySelector("#answer4")
+    button4: document.querySelector("#answer4")
 };
-
-// LOGIC
-// click the start button
-//     timer starts
-//     I am presented with a question
 
 startButton.addEventListener("click", function() {
     questionBox.textContent = (JSON.stringify(quizQuestions.q1));
@@ -87,33 +83,49 @@ for(i in Object.entries(buttons)){
             answerPopulate2();
         }else if(clickCount === 3){
             answerPopulate3();
+        }
+    });
+    // Wins
+    buttons.button2.addEventListener("click", function(){
+        if(buttons.button2.textContent === quizChoices.choices1[1]){
+            wins.textContent = 1;
+        }else if(buttons.button2.textContent === quizChoices.choices4[1]){
+            wins.textContent = 4;
         };
     });
-};
+    buttons.button4.addEventListener("click", function(){
+        if(buttons.button4.textContent === quizChoices.choices2[3]){
+            wins.textContent = 2;
+        };
+    });
+    buttons.button3.addEventListener("click", function(){
+        if(buttons.button3.textContent === quizChoices.choices3[2]){
+            wins.textContent = 3;
+        };
+    });
+    // Losses
 
-// if i answer a question incorrectly
-// function answerResult() {
-//     Object.entries(buttons)[i][1].addEventListener("click", function() {
 
+// List of what I need
+// I need to log wins and losses when I click the right or wrong answer
+// I need to log the results, the user's initials, and capture the data
+// I need to be able to reset the timer when I answer a question
+// I need to reset the time and stop the game if the user does not answer a question in time
+
+// buttons.button2.addEventListener("click", function() {
+//     if(buttons.button2.textContent === quizChoices.choices1[1]){
+//         wins.textContent = ++winsCount;
 //     }
-//     })
-// }
+// });
+// // if i answer a question incorrectly
+// if(Object.entries(buttons)[1][0].textContent === Object.entries(quizChoices)[0][1][1]){
+//     buttons.button2.addEventListener("click", function() {
+//         wins.TextContent = winsCount = 1;
+//     });
+// };
+
 
 // if all questions are answered or the timer reaches 0
 //     game is over
 //         ask for user initials
 //         save initials and score
-
-// HTML Elements
-// start button
-//     title and explanation
-
-// timer area
-
-// question area
-//     answer area
-//     4 different answer buttons
-
-// score area
-
-// game over area
